e6311cbba3612f4ee189adc4003caeb9b331cdd2
WATANABE Shin <sin9270@gmail.com>
2022-08-09 00:36:24 +0900

    temporarily add

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name="cloudwatch-logs-insights-url-encoder",
+    packages=["cloudwatch_logs_insights_url_encoder"],
+    version="0.1.0",
+    license="MIT",
+    description="Simple module to generate cloudwatch logs insights URL",
+    author="WATANABE Shin",
+    author_email="sin9270@gmail.com",
+    url="https://github.com/sin9270/cloudwatch-logs-insights-url-encoder",
+    download_url="https://github.com/sin9270/cloudwatch-logs-insights-url-encoder/archive/refs/tags/0.1.0.tar.gz",
+    keywords=[],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
